94443893b85d99b9543ff39a8dda47bb9121ee71
jpasao <jpsolivares@gmail.com>
2021-04-09 21:46:59 +0200

    Corporate colors and logos

--- application/view/admin/login/index.php
+++ application/view/admin/login/index.php
@@ -23,6 +23,7 @@
         <!-- Stylesheet -->
         <link rel="stylesheet" href="<?php echo URL; ?>assets/admin/vendors/css/base/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo URL; ?>assets/admin/vendors/css/base/elisyam-1.5.min.css">
+        <link rel="stylesheet" href="<?php echo URL; ?>assets/css/custom.css">
         <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
